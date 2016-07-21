8513bc53274a2f575ea56e8973a4e23bdf6279e1
Jeffrey Tai <jeffreytai@ucla.edu>
2016-07-21 16:35:51 -0700

    map and transit time

--- index.php
+++ index.php
@@ -19,36 +19,77 @@ require 'class.iCalReader.php';
   <script type="text/javascript" src="js/weather.js" async></script>
   <!-- <script type="text/javascript" src="js/rssfeed.js" async defer></script> -->
 
-  <script src="http://maps.googleapis.com/maps/api/js" async></script>
-  <script>
+  <!-- <script src="http://maps.googleapis.com/maps/api/js"></script> -->
+  <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js" async>
+
     function initMap() {
+
+      var xmlhttp = new XMLHttpRequest();
+      var gmaps_api_key = 'AIzaSyBnMRgsLkIVjd1wVOETpKdHY9XYkqf4lS0';
+      var url = 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=34.067297,-118.452532&destinations=34.046519,-118.250408&key=';
+
+      xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 & xmlhttp.status == 200) {
+          var obj = JSON.parse(xmlhttp.responseText);
+          var time = obj.rows[0].elements[0].duration.text;
+          document.getElementById('travel-time').innerHTML = time;
+          document.getElementById('travel-time').innerHTML += ' to work';
+          // console.log(time);
+        }
+      }
+      xmlhttp.open('GET', url + gmaps_api_key, true);
+      xmlhttp.send();
+
+
+      var directionsDisplay;
+
       // Instantiate a directions service
       var directionsService = new google.maps.DirectionsService;
+      var map;
 
-      // Create a map and center it
-      var mapProp = {
-        center:new google.maps.LatLng(34.067342, -118.453143),
-        zoom:12,
-        // mapTypeId:google.maps.MapTypeId.ROADMAP,
-        mapTypeControl:false,
-        disableDefaultUI:true
-      };
+      function initialize() {
+        directionsDisplay = new google.maps.DirectionsRenderer;
+        var apartment = new google.maps.LatLng(34.067342, -118.452961);
+        var mapOptions = {
+          zoom: 14,
+          center: apartment,
+          zoomControl: false,
+          streetViewControl: false
+        };
+        map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+        directionsDisplay.setMap(map);
+        calcRoute();
+      }
 
-      // Create a renderer for directions and bind it to the map.
-      var directionsDisplay = new google.maps.DirectionsRenderer({map: map});
+      function calcRoute() {
+        var start = new google.maps.LatLng(34.067342, -118.452961);
+        var end = new google.maps.LatLng(34.046670, -118.251095);
 
-      directionsService.route({
-        origin: '540 Kelton Ave, Los Angeles, CA 90024',
-        destination: '215 W 6th St, Los Angeles, CA 90014',
-        travelMode: google.maps.TravelMode.DRIVING
-      }, function(response, status) {
-        if (status === google.maps.DirectionsStatus.OK) {
-          directionsDisplay.setDirections(response);
-        }
-      })
-      var map=new google.maps.Map(document.getElementById("map"),mapProp);
+        var bounds = new google.maps.LatLngBounds();
+        bounds.extend(start);
+        bounds.extend(end);
+        map.fitBounds(bounds);
+        var request = {
+          // zoomControl: false,
+          origin: start,
+          destination: end,
+          travelMode: google.maps.TravelMode.DRIVING,
+        };
+        directionsService.route(request, function(response, status) {
+          if (status == google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(response);
+            directionsDisplay.setMap(map);
+          } else {
+            alert("Error getting directions");
+          }
+        });
+      }
+
+      google.maps.event.addDomListener(window, 'load', initialize);
+
     }
-    google.maps.event.addDomListener(window, 'load', initMap);
+    initMap();
+
   </script>
 
 </head>
@@ -92,7 +133,8 @@ require 'class.iCalReader.php';
   <div class="region upper third"><div class="container"></div></div>
   <div class="region middle center">
     <div class="container">
-      <div id="map"></div>
+      <div id="map-canvas"></div>
+      <div id="travel-time" class="small"></div>
     </div>
   </div>
 	<div class="region lower third"><div class="container"><br/></div></div>
